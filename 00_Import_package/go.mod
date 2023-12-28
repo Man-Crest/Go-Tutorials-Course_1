08f4658d9800e9930dc99b88064108e37e3201df
man_crest <man.m@crestinfosystems.com>
2023-12-28 15:13:19 +0530

    tags are added in this commit

---
+++ 00_Import_package/go.mod
@@ -0,0 +1,7 @@
+module import_package
+
+go 1.21.5
+
+require github.com/Man-Crest/Puppy-package v0.0.0-20231228085223-7fc2843ff6a5
+
+require github.com/Man-Crest/Dog v0.0.0-20231228084757-8227f6e3287a // indirect
---
+++ 00_Import_package/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Man-Crest/Puppy-package"
+)
+
+func main() {
+	s1 := Puppy.Bark()
+	// s2 := Puppy-package.Barks()
+
+	fmt.Println(Puppy.Barks())
+	fmt.Println(Puppy.Aware())
+	fmt.Println(s1)
+
+}
